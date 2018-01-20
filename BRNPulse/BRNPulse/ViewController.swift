d608374409fb77c1a5f578234f6d3b476f5f30cc
Naveen <pnaveen30@achiever-129.local>
2018-01-20 18:55:49 +0530

    created new file

---
+++ BRNPulse/BRNPulse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BRNPulse
+//
+//  Created by Naveen on 1/20/18.
+//  Copyright © 2018 Naveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
